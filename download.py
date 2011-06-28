24efbb046e75d7d7caaab4fcb9fdb031b19291ee
Dylan Bowman <dbowman6@sbcglobal.net>
2011-06-28 14:22:11 -0700

    Edited download.py via GitHub

--- download.py
+++ download.py
@@ -1,6 +1,5 @@
 #Download and place the current settings.
 import urllib
-##urllib.urlretrieve ("http://nvbowler.com/mcserver/current.zip", "./current.zip")
 fileHandleLocal = open("current.zip","wb")
 fileHandleWeb = urllib.urlopen("Zip File Location", "rb").read()
 fileHandleLocal.write(fileHandleWeb)
